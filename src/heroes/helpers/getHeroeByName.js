38adce315a26ef3966083cb0f6aa2405fb9c7435
Danna Castro <castrocardenasdanna.10@gmail.com>
2025-05-16 18:06:13 -0500

    final

--- src/heroes/helpers/getHeroeByName.js
+++ src/heroes/helpers/getHeroeByName.js
@@ -1,5 +1,9 @@
 import { heroes } from "../data";
 
-export const getHeroeByName = (name) => {
-    return heroes.find( hero => hero.superhero.toLowerCase() === name.toLowerCase() );
+export const getHeroeByName = (name = '') => {
+    name = name.toLocaleLowerCase().trim();
+
+    if (name.length === 0) return [];
+    return heroes.filter(
+        hero => hero.superhero.toLocaleLowerCase().includes(name));
 }
--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,4 +1,35 @@
+import { useNavigate, useLocation } from "react-router-dom";
+import { useForm } from "../../hooks";
+import queryString from "query-string";
+import { getHeroeByName } from "../helpers";
+import { HeroCard } from '../components';
+
+
+
 export const SearchPage = () => {
+
+    const navigate = useNavigate()
+    const location = useLocation()
+
+    const {q=''} = queryString.parse(location.search);
+    //console.log(q);
+
+    const heroes = getHeroeByName(q);
+    const {searchText, onInputChange} = useForm(
+        {
+        searchText: ''
+        }
+    )
+
+    const showSearch = (q.length === 0);
+    const showError = (q.length > 0) && heroes.length === 0;
+
+    const onSearchSubmit = (event) => {
+        event.preventDefault();
+        if(searchText.trim().length <= 1) return;
+        navigate(`?q=${ searchText }`);
+    }
+
     return ( 
         <>
             <h1>SearchePage</h1>
@@ -7,12 +38,14 @@ export const SearchPage = () => {
                 <div className="col-5">
                     <h4>Busqueda</h4>
                     <hr />
-                    <form>
+                    <form onSubmit={ onSearchSubmit }>
                         <input type="text" 
                         placeholder="Buscar un heroe"
                         className="form-control"
                         name="searchText"
                         autoComplete="off"
+                        value={ searchText }
+                        onChange={ onInputChange }
                         />
                         <button className="btn btn-outline-primary mt-2">
                             Buscar
@@ -21,13 +54,21 @@ export const SearchPage = () => {
                 </div>
                 <div className="col-7">
                     <h4>Resultados</h4>
-                    <div className="alert alert-primary">
-                        Buscando un heroe
-                        </div>
-                    <div className="alert alert-danger">
-                        No encontramos ningun heroe
-                        </div>  
 
+                    {/* {
+                        (q === '') 
+                        ? <div className="alert alert-primary" >Buscar un heroe</div>
+                        : (heroes.length === 0) && <div className="alert alert-danger">No hay resultados con <b>{ q }</b></div>
+                    } */}
+                    
+                    <div className="alert alert-primary animate__animated animate__backInRight" style={{display: showSearch ? '' : 'none'}}>Buscar un heroe</div>
+                    <div className="alert alert-danger animate__animated animate__backInRight"style={{display: showError ? '' : 'none'}}>No hay resultados con <b>{ q }</b></div>
+
+                    {
+                        heroes.map( hero => (
+                            <HeroCard key={hero.id} {...hero} />
+                        ))
+                    }
 
 
                 </div>
